f39ff8ceaa27b78bfd870433b0b0ad263a01bd0d
Apoorv Umang <apoorvumang@gmail.com>
2015-07-28 18:14:10 +0530

    fixed some corner cases

--- Fig2Word.groovy
+++ Fig2Word.groovy
@@ -1,14 +1,14 @@
 def fig2word(int num) {
-    def units = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
-    def teens = ["","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen",
+    units = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+    teens = ["","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen",
                  "Eighteen","Nineteen"]
-    def tens = ["","Ten","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", 
+    tens = ["","Ten","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", 
                 "Ninety"]
-    def thousands = ["", "Thousand","Lakh","Crore"]
+    thousands = ["", "Thousand","Lakh","Crore"]
     if (num==0)
         "Zero"
     else {
-        def answer = []
+        answer = []
         numStr = ("0000000000" + num.toString()).reverse()
         def tensAndUnits = {
             ans = []
@@ -32,7 +32,7 @@ def fig2word(int num) {
             t = numStr[i + 1].toInteger();
             u = numStr[i].toInteger();
             curAns << tensAndUnits()
-            curAns << thousands[c];
+            if(t > 0 || u > 0) curAns << thousands[c];
             answer = curAns + answer;
             c = c + 1
         }
@@ -43,7 +43,7 @@ def fig2word(int num) {
         u = numStr[length - 1].toInteger()
         if (h > 0)
             answer << units[h] << "Hundred"
-        if((u > 1 || t > 1) && h > 0)
+        if(((u >= 1 || t >= 1) && h > 0)||(num >= 1000))
             answer << "and"
         answer << tensAndUnits()
         answer.flatten().join(" ")
