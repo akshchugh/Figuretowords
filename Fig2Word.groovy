ca469609710023d9dd8d45a56ef866b577645a7e
Apoorv Umang <apoorvumang@gmail.com>
2015-07-28 17:32:31 +0530

    working code

--- Fig2Word.groovy
+++ Fig2Word.groovy
@@ -1,35 +1,60 @@
 def fig2word(int num) {
-	def units = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
-	def teens = ["","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen",
-				 "Eighteen","Nineteen"]
-	def tens = ["","Ten","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", 
-				"Ninety"]
-	def thousands = ["", "Thousand","Lakh","Crore"]
-	if (num==0)
-		"Zero"
-	else {
-		String answer = ""
-		length = num.toString().size()
-		h = (num\\100)%10;
-		t = (num\\10)%10;
-		u = num%10;
-		if (h > 0)
-			answer+= units[h] + " hundred"
-		if (t > 1) {
-			answer += tens[t]
-			if(u >= 1)
-				answer += " " + units[u]
-		}
-		else if(t == 1){
-			if(u >= 1) answer += teens[u]
-			else answer += tens[t]
-		}
-		else {
-			if(u >= 1) answer += units[u]
-		}
-		println answer
-	}
-	"Six hundred"
+    def units = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+    def teens = ["","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen",
+                 "Eighteen","Nineteen"]
+    def tens = ["","Ten","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", 
+                "Ninety"]
+    def thousands = ["", "Thousand","Lakh","Crore"]
+    if (num==0)
+        "Zero"
+    else {
+        def answer = []
+        numStr = num.toString()
+        origSize = num.toString().size()
+        numStr = "0000000000" + numStr;
+        length = numStr.size()
+       
+        c = 1
+        for(i = 4; i <= origSize; i+=2) {
+            curAns = [];
+            t = numStr[length - i - 1].toInteger();
+            u = numStr[length - i].toInteger();
+             if (t > 1) {
+                curAns << tens[t]
+                if(u >= 1) curAns << units[u]
+            }
+            else if(t == 1){
+                if(u >= 1) curAns << teens[u]
+                else curAns << tens[t]
+            }
+            else {
+                if(u >= 1) curAns << units[u]
+            }
+            curAns << thousands[c];
+            answer = curAns + answer;
+            c = c + 1
+        }
+        
+        h = numStr[length - 3].toInteger()
+        t = numStr[length - 2].toInteger()
+        u = numStr[length - 1].toInteger()
+        if (h > 0)
+            answer << units[h] << "Hundred"
+        if((u > 1 || t > 1) && h > 0)
+            answer << "and"
+        if (t > 1) {
+            answer << tens[t]
+            if(u >= 1) answer << units[u]
+        }
+        else if(t == 1){
+            if(u >= 1) answer << teens[u]
+            else answer << tens[t]
+        }
+        else {
+            if(u >= 1) answer << units[u]
+        }
+        answer.join(" ")
+    }
 }
-def x = 1234
+def x = 54
 println fig2word(x)
